953765e9f51f0f7ce81b839ab5e11b26739f24fd
Karl Stolley <karl.stolley@gmail.com>
2021-11-15 16:43:38 -0600

    Make a named heading-click callback

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@ document.querySelector('html').className = 'js';
 
 var h1 = document.querySelector('h1');
 
-h1.addEventListener('click', function(event) {
-  event.target.innerText = 'Hello from targeted JavaScript';
-});
+h1.addEventListener('click', handleHeadingClick);
+
+function handleHeadingClick(event) {
+  event.target.innerText = 'Hello from non-anonymous targeted JavaScript';
+}
