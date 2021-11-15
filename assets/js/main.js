3169212358ebba113bcb0aae8d8a2c1ec16982eb
Karl Stolley <karl.stolley@gmail.com>
2021-11-15 16:40:13 -0600

    Switch h1 text when clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,6 @@ document.querySelector('html').className = 'js';
 
 var h1 = document.querySelector('h1');
 
-h1.addEventListener('click', function() {
-  console.log('OMG heard the click event on h1!!!');
+h1.addEventListener('click', function(event) {
+  event.target.innerText = 'Hello from targeted JavaScript';
 });
