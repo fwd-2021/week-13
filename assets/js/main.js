db7ceee0b311d00aab8d60373e87786eba204bb3
Karl Stolley <karl.stolley@gmail.com>
2021-11-17 16:04:16 -0600

    Protect h1 nonsense on the home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
-var h1 = document.querySelector('h1');
+var h1 = document.querySelector('#home h1');
 
 document.querySelector('html').className = 'js';
 
-h1.addEventListener('click', handleHeadingClick);
+if (h1) {
+  // Only add this event listener on #home page
+  h1.addEventListener('click', handleHeadingClick);
+}
 
 function handleHeadingClick(event) {
   var heading = event.target;
